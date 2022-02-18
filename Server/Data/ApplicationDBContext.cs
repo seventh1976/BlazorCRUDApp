3a9fa67ef944d44cce7c5fa398d84f5dd88a9980
TUF <TUF@DESKTOP-9QKC3QK>
2022-02-18 11:10:53 +0800

    Add project files.

---
+++ Server/Data/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyFirstBlazorApp.Shared.Models;
+
+namespace MyFirstBlazorApp.Server.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
